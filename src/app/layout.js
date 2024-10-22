fbc1d9930f364267250770b12949f279300e3922
Oktay Aydoğan <31257020+oktayaydogan@users.noreply.github.com>
2024-10-22 20:41:25 +0300

    Refactor site.webmanifest and manifest.js files

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="tr">
+			<link
+				rel="icon"
+				type="image/png"
+				href="/favicon-48x48.png"
+				sizes="48x48"
+			/>
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<meta name="apple-mobile-web-app-title" content="Boxify" />
+			<link rel="manifest" href="/site.webmanifest" />
 			<body className="antialiased mt-20 bg-gray-100">
 				<ClientWrapper>
 					<Navbar />
